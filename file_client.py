6ca18a9d1f50121a1e948bb52526d794bbfd08b3
Intizar Khilji <inkhilji@gmail.com>
2020-08-04 11:54:16 +0530

    version 1.0

--- file_client.py
+++ file_client.py
@@ -1,14 +1,19 @@
+# A file transfer program called [FILER 1.0]
 # (c) Intzaar
 # File Transfer using python3
-# file_client 0.2
-# ---------------------------------
+# file_client 1.0
+# Date - 02-08-2020
+# -----------Variables-------------
 host = 'localhost'
 port = 9999
 path = ''
+seperator_len = 30
 # ---------------------------------
 
+print('-'*seperator_len+'\n\tFILER\n'+'-'*seperator_len)
+# Python imports
 import socket, os, sys,time
-
+# Manage command line arguments
 args = sys.argv
 if len(args)==2:
     host = args[1]
@@ -16,13 +21,12 @@ elif len(args)==3:
     host = args[1]
     path = args[2]
 else:
-    print('args[1] - Host IP Address\nargs[2] - File Path\nDefault IP Address - localhost\n\n')
-
+    print('-'*seperator_len+'\nargs[1] - Host IP Address\nargs[2] - File Path\nDefault IP Address - localhost\n'+'-'*seperator_len)
+# Progress Bar
 def progress(count, total):
     status = ''
-    bar_len = 60
+    bar_len = 50
     filled_len = int(round(bar_len * count / float(total)))
-
     percents = round(100.0 * count / float(total), 1)
     bar = '#' * filled_len + '.' * (bar_len - filled_len)
     if percents < 100:
@@ -31,24 +35,26 @@ def progress(count, total):
         status = 'Done.           '
     sys.stdout.write('[%s] %s%s | %s\r' % (bar, percents, '%', status))
     sys.stdout.flush()
-
+# Establish connection
 def make_connection(host,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('[+] Waiting for server...')
     i,n = 0,5
     while i<n:
         try:
             s.connect((host, port))
             break
         except:
-            print(f'[-] Retrying({i+1})...')
+            sys.stdout.write('[-] Retrying('+str(i+1)+')...\r')
+            sys.stdout.flush()
             time.sleep(0.5)
             i+=1
             if i == n:
-                print('[-] Not connected.')
+                print('\n[-] Not connected.')
                 sys.exit()
     print(s.recv(32).decode())
     return s
-
+# File receiver logic
 def recv_file(s,path=''):
     sep='<SEP>'
     info = s.recv(1024).decode()
@@ -64,19 +70,27 @@ def recv_file(s,path=''):
             line = s.recv(buffer)
             f.write(line)
         print()
+        f.seek(0)
+        f.flush()
     filesize2 = os.path.getsize(file)
-    print('Received File Size :',filesize2,'Bytes')
     if filesize == filesize2:
         s.send(b'ack')
         print('[+] Transmission Successfully Completed.\n')
     else:
         print('[-] The received file may be corrupted.')
-    s.close()
+        print('[-] Received File Size :',filesize2,'Bytes')
+    
+def main():
+        try:
+            s = make_connection(host,port)
+            number_of_files = int(s.recv(16).decode().strip())
+            print('[+] Number of files :', number_of_files)
+            for _ in range(number_of_files):
+                recv_file(s,path)
+        except Exception as e:
+            s.close()
+            print('[-]',e)
+        s.close()
 
 if __name__ == '__main__':
-    try:
-        s = make_connection(host,port)
-        recv_file(s,path)
-    except Exception as e:
-        s.close()
-        print('[-]',e)
+    main()
--- file_server.py
+++ file_server.py
@@ -1,33 +1,52 @@
+# A file transfer program called [FILER 1.0]
 # (c) Intzaar
 # File Transfer using python3
-# file_server 0.2
-# ---------------------------------
+# file_server 1.0
+# Date - 02-08-2020
+# ----------Variables--------------
 host = ''
 port = 9999
 buffer = 64
+seperator_len = 30
+files = []
 # ---------------------------------
 
+print('-'*seperator_len+'\n\tFILER\n'+'-'*seperator_len)
+# Python imports
 import socket, os, sys
+# Finding IP address
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as f:
+        f.connect(('8.8.8.8',53))
+        print('-'*seperator_len+'\nHost IP Address :',f.getsockname()[0]+'\n'+'-'*seperator_len+'\n')
+except:
+    print('-'*seperator_len+'\nYou are not connected yet\nLocalhost IP - 127.0.0.1')
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as f:
-    f.connect(('8.8.8.8',53))
-    print('Host IP Address :',f.getsockname()[0])
-
+# Manage command line arguments
 args = sys.argv
-if len(args) == 1:
-    print('args[1] - File Name\nargs[2] - Buffer Size')
-    sys.exit()
-elif len(args) == 2:
-    file = args[1]
-elif len(args) == 3:
-    file = args[1]
+if len(args) == 3:
+    files = [args[1]]
     buffer = int(args[2])
+elif '-f' in args or '-b' in args:
+    if '-f' in args: 
+        files = [x for x in args if os.path.isfile(x)]
+        files = files[1:]
+    if '-b' in args:
+        buffer = int(args[args.index('-b')+1])
+else:
+    print('-'*seperator_len)
+    print('For single file')
+    print('args[1] - File Name\nargs[2] - Buffer Size')
+    print('For multiple files')
+    print('-f - Filenames\n-b - Buffer Size')
+    print('-'*seperator_len)
+    sys.exit()
 
+# Progress Bar
 def progress(count, total):
     status = ''
-    bar_len = 60
+    bar_len = 50
     filled_len = int(round(bar_len * count / float(total)))
-
     percents = round(100.0 * count / float(total), 1)
     bar = '#' * filled_len + '.' * (bar_len - filled_len)
     if percents < 100:
@@ -37,8 +56,10 @@ def progress(count, total):
     sys.stdout.write('[%s] %s%s | %s\r' % (bar, percents, '%', status))
     sys.stdout.flush()
 
+# Make connection
 def make_connection(host,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(20)
     s.bind((host,port))
     s.listen(1)
     print('[+] Waiting for connection...')
@@ -47,6 +68,7 @@ def make_connection(host,port):
     c.send(b'[+] Connection established.')
     return c
 
+# File transfer logic
 def send_file(c, file, buffer):
     sep='<SEP>'
     basename = os.path.basename(file)
@@ -66,12 +88,22 @@ def send_file(c, file, buffer):
             print('[+] File Transfered.\n')
     except Exception as e:
         print('[-] Acknowledgement not received.\n'+e)
-    c.close()
+
+def main():
+        try:
+            c = make_connection(host,port)
+            number_of_files = str(len(files))
+            print('[+] Number of files :', number_of_files)
+            c.send(number_of_files.encode())
+            for fle in files:
+                send_file(c,fle,buffer)
+        except Exception as e:
+            try:
+                c.close()
+            except:
+                print('[-] Not Connected.')
+            print('[-]',e)
+        c.close()
 
 if __name__ == '__main__':
-    try:
-        c = make_connection(host,port)
-        send_file(c,file,buffer)
-    except Exception as e:
-        c.close()
-        print('[-]',e)
+    main()
